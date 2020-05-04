4431c42a96642e3b3fb6b78578c738e1a52df5f5
stomita083 <s-tomita@unext.jp>
2020-05-04 21:46:10 +0900

    Heroku対応

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,29 +1,26 @@
 'use strict';
 
 const Sequelize = require('sequelize');
-// const sequelize = new Sequelize(
-//   'postgres://postgres:postgres@localhost/schedule_arranger'
-// );
 
 // 接続先情報
-const host     = 'localhost';
+const host = 'localhost';
 const database = 'schedule_arranger';
 const username = 'app';
 const password = 'app';
 
 const sequelize = new Sequelize(database, username, password, {
-    host: host,
-    dialect: 'mysql',
-    operatorsAliases: false,
-    pool: {
-      min: 0,
-      max: 5,
-      acuire: 30000,
-      idle  : 10000
-    }
+  host: host,
+  dialect: 'mysql',
+  operatorsAliases: false,
+  pool: {
+    min: 0,
+    max: 5,
+    acuire: 30000,
+    idle: 10000,
+  },
 });
 
 module.exports = {
   database: sequelize,
-  Sequelize: Sequelize
+  Sequelize: Sequelize,
 };
